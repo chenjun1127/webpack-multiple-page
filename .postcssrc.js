f835e1ade844a2d3ce86f5a3e761261bf6fd1f81
Jone-Chen <kiss19861127@126.com>
2018-08-30 17:08:20 +0800

    Update .postcssrc.js

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,6 +1,8 @@
 //.postcssrc.js或postcss.config.js
 module.exports = {
-    "plugins": [
-        require('autoprefixer')
+    plugins: [
+        require('autoprefixer')({
+            browsers: ['cover 99.5% in CN']
+        })
     ]
 }
